7b0a260daa9a6e515d033c9596a24f80859d6666
abdelkrim-joudri <abdelkrimjoudri@gmail.com>
2024-04-15 23:22:19 +0100

    checking once again that the push is done using my account , not that of anas's

--- src/main/java/com/example/jobmarket/Controllers/TestController.java
+++ src/main/java/com/example/jobmarket/Controllers/TestController.java
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
     @GetMapping("/test")
     public String test(){
-        return "it's working";
+        return "it's working ";
     }
 }
