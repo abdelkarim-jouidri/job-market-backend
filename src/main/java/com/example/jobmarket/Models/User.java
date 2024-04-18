2e607f797da7b10a35415cc719026ed24430db88
abdelkrim-joudri <abdelkrimjoudri@gmail.com>
2024-04-18 02:33:31 +0100

    added user entity

--- src/main/java/com/example/jobmarket/Models/User.java
+++ src/main/java/com/example/jobmarket/Models/User.java
@@ -1,10 +1,68 @@
 package com.example.jobmarket.Models;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+import java.security.Principal;
+import java.util.Collection;
 
 @Entity
-public class User {
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class User implements UserDetails , Principal {
     @Id
+    @GeneratedValue
     Integer id;
+
+    String email;
+
+    String password;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return null;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
+
+    @Override
+    public String getName() {
+        return null;
+    }
 }
